65662214f916e6887ef484d7500cd4c060926896
Hui Teng Lim <huiteng1995@gmail.com>
2022-10-28 15:10:00 +0100

    Added bootstrap, updated sass directory and added app-header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Import components
+import {
+  AppHeaderComponent
+} from './components';
+const APP_COMPONENTS = [
+  AppHeaderComponent
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...APP_COMPONENTS
   ],
   imports: [
     BrowserModule
